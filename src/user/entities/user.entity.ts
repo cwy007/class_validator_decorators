3c9c8e04c8f2c4dd51a1fb3e83f87d7112ec3757
chanweiyan <chanweiyan007@gmail.com>
2026-05-10 17:58:47 +0800

    fix: add class-transformer decorators to user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,10 +1,19 @@
+import { Exclude, Expose, Transform } from "class-transformer";
+
 export class User {
   id: number;
 
   username: string;
 
+  @Exclude()
   password: string;
 
+  @Expose()
+  get xxx() {
+    return `${this.username} (${this.email})`;
+  }
+
+  @Transform(({ value }) => `email: ${value}`)
   email: string;
 
   constructor(partial: Partial<User>) {
