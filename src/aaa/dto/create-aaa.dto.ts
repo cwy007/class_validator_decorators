4443e55548f9d7fd59053ad9a68bf0ff7c42c1e3
chanweiyan <chanweiyan007@gmail.com>
2026-05-10 15:50:55 +0800

    fix: test some internal validation decorators

--- src/aaa/dto/create-aaa.dto.ts
+++ src/aaa/dto/create-aaa.dto.ts
@@ -1,1 +1,9 @@
-export class CreateAaaDto {}
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateAaaDto {
+
+  @IsNotEmpty({ message: "aaa不能为空" })
+  @IsString({ message: "aaa必须是字符串" })
+  @IsEmail({}, { message: "aaa必须是一个有效的邮箱地址" })
+  aaa: string;
+}
